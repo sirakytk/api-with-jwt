59ba2d84c6c68c1d6dab260a03dfeda752828730
sirakytk <sirakytk@gmail.com>
2025-08-18 21:53:00 +0700

    panggil dari .env

--- databases/db.go
+++ databases/db.go
@@ -1,8 +1,11 @@
 package databases
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/sirakytk/api-with-go/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,12 +14,28 @@ import (
 var DB *gorm.DB
 
 func InitializeDB() {
-	dsn := "host=localhost user=postgres password=postgres404 dbname=go_jwt_auth port=5432 sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Gagal membaca file .env")
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbName, port)
+
+	// Koneksi ke DB
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("koneksi database gagal:")
+		log.Fatal("Koneksi database gagal:", err)
 	}
 
 	DB = db
+
+	// Auto migrate model
 	db.AutoMigrate(&models.User{})
 }
