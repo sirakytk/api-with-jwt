43f8e624b25ce0b34942aa815f03de80e77aa232
sirakytk <sirakytk@gmail.com>
2025-08-20 16:34:02 +0700

    add rout untuk tampilan logi, register, dan lain2

--- routes/route.go
+++ routes/route.go
@@ -10,4 +10,8 @@ func Routes(app *fiber.App) {
 	app.Post("/api/login", controllers.Login)
 	app.Post("/api/logout", controllers.Logout)
 	app.Get("/api/user", controllers.GetUser)
+	app.Get("/", controllers.Welcome)
+	app.Get("/login", controllers.LoginPage)
+	app.Get("/register", controllers.RegisterPage)
+	app.Get("/home", controllers.HomePage)
 }
