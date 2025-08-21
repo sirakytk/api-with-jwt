968ce94c66c1ad1289312aee8d8ac3874db7845c
sirakytk <sirakytk@gmail.com>
2025-08-21 07:42:46 +0700

    user controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,20 +1,20 @@
 package controllers
 
-import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/sirakytk/api-with-go/databases"
-	"github.com/sirakytk/api-with-go/models"
-)
+// import (
+// 	"github.com/gofiber/fiber/v2"
+// 	"github.com/sirakytk/api-with-go/databases"
+// 	"github.com/sirakytk/api-with-go/models"
+// )
 
-func GetUser(c *fiber.Ctx) error {
-	var user models.User
-	userID := c.Locals("userID")
+// func GetUser(c *fiber.Ctx) error {
+// 	var user models.User
+// 	userID := c.Locals("userID")
 
-	if err := databases.DB.First(&user, userID).Error; err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"message": "User not found",
-		})
-	}
+// 	if err := databases.DB.First(&user, userID).Error; err != nil {
+// 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+// 			"message": "User not found",
+// 		})
+// 	}
 
-	return c.JSON(user)
-}
+// 	return c.JSON(user)
+// }
